b29d3d1f6c84878a5322de60797bf50bb037e211
zeromo0713 <157686746+zeromo0713@users.noreply.github.com>
2024-01-26 17:39:59 +0900

    excel

--- gitexam/gittest.py
+++ gitexam/gittest.py
@@ -1,4 +1,10 @@
-import os
+import openpyxl
 
-print("git 실험")
-print("마지막 수정")
+workbook = openpyxl.load_workbook('C:\\Users\\user\\Desktop\\새 폴더\\입력하나만.xlsx')
+
+ws = workbook.active
+
+list_data = ['나는영모',27,98]
+ws.append(list_data)
+
+workbook.save('C:\\Users\\user\\Desktop\\새 폴더\\입력하나만.xlsx')
